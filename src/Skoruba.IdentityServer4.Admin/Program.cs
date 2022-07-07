c8dcaabc940acee80716e6e9217fb1941a4a4f4f
zatkhahi <m.zatkhahi@gmail.com>
2022-07-07 13:41:36 +0430

    message added

--- src/Skoruba.IdentityServer4.Admin/Program.cs
+++ src/Skoruba.IdentityServer4.Admin/Program.cs
@@ -34,6 +34,7 @@ namespace Skoruba.IdentityServer4.Admin
 
                 await ApplyDbMigrationsWithDataSeedAsync(args, configuration, host);
 
+                Log.Information("Migration applied successfully");
                 host.Run();
             }
             catch (Exception ex)
